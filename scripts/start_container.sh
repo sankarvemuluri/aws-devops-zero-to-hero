3f7acb819932caaeeae34b27721978ef9de63221
sankarv <sankarvemuluri@gmail.com>
2024-09-30 14:28:54 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,6 +4,8 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull abhishekf5/simple-python-flask-app
 
+docker stop nginx ; docker rm nginx
+
 # Run the Docker image as a container
 #docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
-docker run -it -d -P nginx 
+docker run -it -d -p 80:80 --name nginx nginx 
